624b903543770c48fdec6bba80c0999c26de1466
starman <ciemalajakub@gmail.com>
2022-07-18 11:45:20 +0200

    Add images to home page

--- lib/home.dart
+++ lib/home.dart
@@ -7,16 +7,29 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar('Home Page', false),
+      appBar: CustomAppBar('', false),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Image.asset('assets/images/logo.png'),
+            const SizedBox(height: 24),
+            Image.asset('assets/images/qrcodebusiness.png'),
+            const SizedBox(height: 24),
+            const Text(
+              'We\'re glad you\'re here, \n let\'s get started',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 24),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/login');
               },
-              child: const Text('Log In'),
+              child: const Text('Start'),
             ),
           ],
         ),
